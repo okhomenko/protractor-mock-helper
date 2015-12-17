252bf747a51eba27da33b1cd34196c1134a27897
Oleksandr Khomenko <okhomenko@gmail.com>
2015-12-16 19:54:56 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# protractor-mock-helper
+
+Another protractor http backend mock helper. It uses similar aproach to http-backend-proxy and protractor-http-mock.
+
+
+## Usage
+
+```javascript
+
+describe('Example Test Suite', function () {
+
+  it('should test', function () {
+    browser.addMockModule('httpBackEndMock', mocks.build([
+      function ($httpBackend) {
+        return $httpBackend.whenPOST(/api\/auth$/).respond(200);
+      }
+    ]));
+
+    browser.get('/');
+    element(by.css('[name="username"]')).sendKeys('admin');
+    element(by.css('[name="password"]')).sendKeys('password');
+    element(by.buttonText('OK')).click();
+
+    expect(element(by.css('.messages')).getText()).toMatch('Successfully');
+  });
+
+});
+
+```
+
+LICENSE: MIT
---
+++ index.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var _ = require('underscore');
+
+function options204($httpBackend) {
+  $httpBackend.when('OPTIONS', /\/api\//)
+    .respond(function () {
+      return [204, '', {}];
+    });
+}
+
+function passThrough($httpBackend) {
+  var methods, url;
+
+  url = /^(?!api)/;
+
+  methods = [
+    'HEAD',
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'JSONP'
+  ];
+
+  methods.forEach(function (method) {
+    $httpBackend['when' + method](url).passThrough();
+  });
+}
+
+function build(funcs) {
+  var headStr, headDefineStr,
+    bodyStr, optionsStr, footStr;
+
+  headStr = "angular.module('httpBackEndMock')";
+  headDefineStr = "angular.module('httpBackEndMock', ['ngMockE2E'])";
+
+  optionsStr = "\r.run(" + options204 + ")";
+  footStr = "\r.run(" + passThrough + ")";
+
+  bodyStr = (Array.isArray(funcs) ? funcs : [funcs])
+    .map(function (f) {
+      return "\r.run(" + f + ")";
+    }).join('');
+
+  return Function([
+    'try { ',
+      headDefineStr, bodyStr, optionsStr, footStr,
+    '} catch (err) { ',
+      headStr, bodyStr, optionsStr, footStr,
+    '}'
+  ].join(''));
+}
+
+function verifyNoOutstandingExpectationFn(callback) {
+  var $httpBackend = angular.element('body').injector().get('$httpBackend');
+
+  try {
+    $httpBackend.verifyNoOutstandingExpectation();
+    callback(null);
+  } catch (err) {
+    callback(err.message);
+  }
+}
+
+function verifyNoOutstandingRequestFn(callback) {
+  var $httpBackend = angular.element('body').injector().get('$httpBackend');
+
+  try {
+    $httpBackend.verifyNoOutstandingRequest();
+    callback(null);
+  } catch (err) {
+    callback(err.message);
+  }
+}
+
+function verifyNoOutstandingExpectation() {
+  var message = browser.executeAsyncScript(verifyNoOutstandingExpectationFn);
+  expect(message).toBeNull();
+}
+
+function verifyNoOutstandingRequest() {
+  var message = browser.executeAsyncScript(verifyNoOutstandingRequestFn);
+  expect(message).toBeNull();
+}
+
+function wrapData(func, data) {
+  var s, oldBody, body, fn;
+
+  s = func.toString();
+
+  oldBody = s.substring(s.indexOf('{') + 1, s.lastIndexOf('}') - 1);
+
+  body = '';
+
+  _.each(data, function (value, key) {
+    body += 'var ' + key + ' = ' + JSON.stringify(data[key]) + ';\n';
+  });
+
+  body += '\n' + oldBody;
+
+  fn = Function('$httpBackend', body);
+
+  return fn;
+}
+
+module.exports = {
+  build: build,
+  verifyNoOutstandingExpectation: verifyNoOutstandingExpectation,
+  verifyNoOutstandingRequest: verifyNoOutstandingRequest,
+  wrapData: wrapData
+};
+
